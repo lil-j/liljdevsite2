3e216583d04b2166bda8c8371c413660142e325e
Jacob Harper <jake@krunchy.net>
2020-05-04 01:16:51 -0700

    adds to bio

--- pages/about.js
+++ pages/about.js
@@ -53,7 +53,7 @@ const About = props => (
           <motion.div variants={fadeInUp}>
             <div className="row">
               <div className="col-lg-7 col-sm-12">
-                <h4>I love running, taking photos, making videos, and hanging out with friends- but my true passion is computer programming. After starting with making simple python-based 'bots' using the Discord API, I quickly saw it as a business opportunity, and grew upon that for multiple months. A year later I have expanded into different areas of programming such as full-stack web applications and even working with artificial intelligence.</h4>
+                <h4>I love running, taking photos, making videos, trading stocks, and hanging out with friends- but my true passion is computer programming. After starting with making simple python-based 'bots' using the Discord API, I quickly saw it as a business opportunity, and grew upon that for multiple months. A year later I have expanded into different areas of programming such as full-stack web applications and even working with artificial intelligence. I love Next.Js!</h4>
                 <br/>
                 <h4><img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"/> <a className="text-info" href="https://github.com/lil-j">lil-j</a></h4>
                 <h4 className="text-info"><img className="icon" src="https://image.flaticon.com/icons/svg/2111/2111363.svg"/> lilj#5815</h4>
